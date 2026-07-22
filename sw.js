5e89d7b214bf7867ca02a1b34892e88444e8cf68
dinis-pylypenko <110489152+dinis-pylypenko@users.noreply.github.com>
2026-07-22 10:58:08 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const C = 'gym-v1';
+const FILES = ['./', './index.html', './icon.png', './manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request, {ignoreSearch: true}).then(r => r || fetch(e.request)));
+});
